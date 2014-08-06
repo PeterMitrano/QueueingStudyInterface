f7a3b385be4e527793e9e5896edd93b910a6ffbe
Russell Toris <rctoris@wpi.edu>
2014-08-06 10:32:38 -0400

    first travis test

---
+++ app/Controller/CarlDemoInterfaceController.php
@@ -0,0 +1,29 @@
+<?php
+App::uses('InterfaceController', 'Controller');
+
+/**
+ * CARL Demo Interface Controller
+ *
+ * The CARL demo interface controller. This interface will allow for navigation and manipulation controls.
+ *
+ * @author		Russell Toris - rctoris@wpi.edu
+ * @copyright	2014 Worcester Polytechnic Institute
+ * @link		https://github.com/WPI-RAIL/CarlDemoInterface
+ * @since		CarlDemoInterface v 0.0.1
+ * @version		0.0.1
+ * @package		app.Controller
+ */
+class CarlDemoInterfaceController extends InterfaceController {
+
+/**
+ * The basic view action. All necessary variables are set in the main interface controller.
+ *
+ * @return null
+ */
+	public function view() {
+		// set the title of the HTML page
+		$this->set('title_for_layout', 'CARL (Crowdsourcing for Autonomous Robot Learning)');
+		// we will need some RWT libraries
+		$this->set('rwt', array('roslibjs' => 'current', 'ros3djs' => 'current', 'keyboardteleopjs' => 'current'));
+	}
+}
---
+++ app/View/CarlDemoInterface/view.ctp
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Basic Interface View
+ *
+ * The basic interface displays a camera feed and keyboard teleop.
+ *
+ * @author		Russell Toris - rctoris@wpi.edu
+ * @copyright	2014 Worcester Polytechnic Institute
+ * @link		https://github.com/WPI-RAIL/CarlDemoInterface
+ * @since		CarlDemoInterface v 0.0.1
+ * @version		0.0.1
+ * @package		app.View.CarlDemoInterface
+ */
+?>
+
+<?php echo $this->Rms->ros($environment['Rosbridge']['uri']); ?>
+
+<h1>TEST123</h1>
